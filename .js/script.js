b73858c9f1190ffa9399e7c2b5ab3809f862711e
lv8666302-debug <lv8666302@gmail.com>
2026-09-24 07:47:32 -0300

    save

--- .js/script.js
+++ .js/script.js
@@ -5,16 +5,23 @@ const jump = () => {
     mario.classList.add('jump');
 
     setTimeout(() => {
-
         mario.classList.remove('jump');
-
     }, 500);
 }
 
 const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
-    console.log(pipePosition)
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    console.log(marioPosition;)
+    
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        
+        pipe.style.animation ='none';
+        pipe.style.left = `${pipePosition}px`;
+
+    }
 
 }, 10);
 
